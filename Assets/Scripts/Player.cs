ff686c453c26958bd0d7ba07aebaae860e0ce996
H-Jungjaeheon <jjhking222@naver.com>
2022-01-04 00:21:13 +0900

    초기 프로젝트 파일

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int Hp = 3;
+    public int Damage = 1;
+    public float Speed = 5;
+    public int Boomitem = 3;
+    public int Shielditem = 2;
+    public bool IsShield = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
