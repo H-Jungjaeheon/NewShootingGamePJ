ab1c2fb2c1289192c58c4640ae8fd3633409bbfe
H-Jungjaeheon <jjhking222@naver.com>
2022-01-05 00:11:07 +0900

    Assets&Bullet
    Assets&Bullet1

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int Speed = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBullet();
+        DestroyBullet();
+    }
+    void DestroyBullet()
+    {
+
+    }
+    void MoveBullet()
+    {
+        this.transform.position += new Vector3(0, Speed * Time.deltaTime, 0);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance {get; private set;}
     public int Hp = 3;
     public int Damage = 1;
     public float Speed = 2;
+    float ShootTimer = 0;
+    float ShootDelay = 0.4f;
     public int Boomitem = 3;
     public int Shielditem = 2;
     public bool IsShield = false;
+    public GameObject Bullet;
+    public GameObject Boom;
     float moveX, moveY;
-    
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +30,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
         CameraIn();
+        Attack();
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (Damage < 3)
+            {
+                Damage += 1;
+            }
+            else if(Damage >= 3)
+            {
+                Damage += 0;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        Move();
     }
     public void Move()
     {
@@ -40,5 +64,14 @@ public class Player : MonoBehaviour
         else if (pos.y < 0f) pos.y = 0f;
         else if (pos.y > 1f) pos.y = 1f;
         transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+    public void Attack()
+    {
+        if (Input.GetKey(KeyCode.Z) && ShootTimer >= ShootDelay)
+        {
+            Instantiate(Bullet, this.transform.position, transform.rotation);
+            ShootTimer = 0;
+        }
+        ShootTimer += Time.deltaTime;
     }
 }
